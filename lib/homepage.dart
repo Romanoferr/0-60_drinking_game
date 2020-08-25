d5a5b157a2b55f60368f6de5be80bac27076f51c
Romano Ferreira <arcferreira@id.uff.br>
2020-08-25 13:01:22 -0300

    cross align done

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,16 +5,18 @@ class HomePage extends StatelessWidget {
 
   Widget _contentInHome(){
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(50),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
 
         children: [
           RaisedButton(
             onPressed: () {},
-          )
+            ),
+          SizedBox(height: 150.0),
         ],
-      )
+      ),
       );
   }
 

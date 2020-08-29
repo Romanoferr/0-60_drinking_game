335b69fda6f1240d8d4963712000cc112f0c73db
Romano Ferreira <arcferreira@id.uff.br>
2020-08-29 01:03:58 -0300

    homepage logo

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -6,13 +6,13 @@ class HomePage extends StatelessWidget {
   HomePage({@required this.onGame, @required this.boxCount});
 
   final Function(bool) onGame;
-  final Function (int) boxCount;
+  final Function(int) boxCount;
 
   void newGame() {
     onGame(true);
   }
 
-  void boxCounter(String boxes){
+  void boxCounter(String boxes) {
     print('Number of boxes : $boxes');
     boxCount(int.parse(boxes));
   }
@@ -21,61 +21,65 @@ class HomePage extends StatelessWidget {
 
   Widget _contentInHome() {
     return Padding(
-      padding: EdgeInsets.all(50),
+      padding: EdgeInsets.all(45),
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            HomeButton(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(child: Image.asset("images/drinkg2.png")),
+          HomeButton(
               text: "New Game",
               fontSize: 24,
               onPressed: newGame,
               heightOfset: 50.0,
               borderRadius: 16.0,
-            ),
-            SizedBox(height: 32.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 150, right: 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.center,
-                    controller: myController,
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16.0),
-                        ),
+          ),
+          SizedBox(height: 32.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 150, right: 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  controller: myController,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16.0),
                       ),
-                      labelText: "# Squares",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16.0),
-                        ),
-                        borderSide: BorderSide(
-                          color: Color.fromRGBO(52, 138, 199, 1.0),
-                        ),
+                    ),
+                    labelText: "# Squares",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16.0),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(52, 138, 199, 1.0),
                       ),
                     ),
                   ),
-                  SizedBox(height: 8),
-                  HomeButton(
-                    text: "New Game",
-                    fontSize: 14,
-                    heightOfset: 30.0,
-                    borderRadius: 8.0,
-                    onPressed: () {
-                      boxCounter(myController.text);
-                    },
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(height: 8),
+                HomeButton(
+                  text: "New Game",
+                  fontSize: 14,
+                  heightOfset: 30.0,
+                  borderRadius: 8.0,
+                  onPressed: () {
+                    boxCounter(myController.text);
+                  },
+                ),
+                SizedBox(height: 8),
+              ],
+
             ),
-          ],
+          ),
+        ],
       ),
     );
   }
@@ -83,8 +87,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(37, 187, 216, 1),
       appBar: AppBar(
-          backgroundColor: Color.fromRGBO(20, 93, 108, 1),
+        backgroundColor: Color.fromRGBO(20, 93, 108, 1),
         title: Text("0-25 Drinking Game :)"),
       ),
       body: _contentInHome(),
